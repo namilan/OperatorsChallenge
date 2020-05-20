bd7acc5a87e5aa151445275fe7ca81e19bab010c
namilan <nichole.milan@gmail.com>
2020-05-20 16:04:11 -0400

    challenge questions updated.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	//1) Create a double variable with a value of 20.00
+	//2) Create a second variable of type double with the value of 80.00
+	//3) Add both numbers together and multiply by 100.
+	//4) use the remainder operator to figure out what the remainder from the result of the operation in step 3 and 40.00. 
+	// We used the modulus or remainder operator on ints in the course, but we can also use it on a double
+	//5) Create boolean variable that assigns the value true if the remainder in $4 is 0, or false if it's not zero.
+	//6) Output the boolean variable
+	//7) Write an if-then statement that displays a message "Got some remainder" if the boolean in step 5 is not true. 
+	// Don't be surprised if you see output in this step, where you expect it to not show. 
+
+}
