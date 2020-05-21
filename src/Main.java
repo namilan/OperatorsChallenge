5e01a1be635161d3bbf18c799cda7fc7dd380e75
namilan <nichole.milan@gmail.com>
2020-05-20 20:19:58 -0400

    question 1 and 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 	
+	public static void main(String[] args) {
+	
 	//1) Create a double variable with a value of 20.00
 	//2) Create a second variable of type double with the value of 80.00
 	//3) Add both numbers together and multiply by 100.
@@ -11,4 +13,24 @@ public class Main {
 	//7) Write an if-then statement that displays a message "Got some remainder" if the boolean in step 5 is not true. 
 	// Don't be surprised if you see output in this step, where you expect it to not show. 
 
+	double first_number, second_number, answer;
+	
+	first_number = 20.00;
+	second_number = 80.00;
+	answer = first_number + second_number;
+	System.out.println("Total = " + answer);
+		
+
+//	
+//	result = result * 100;
+//	System.out.println("100 * 100 = " + result);
+//	
+//	result = result % 1000;
+//	System.out.println("1000 % 1000 = " + result);
+//	
+//	result = result % 40; 
+//	System.out.println("1000 % 40 = " + result);
+	
+	}
+	
 }
